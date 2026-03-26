43fa0a2eba33202e85e992eda05f5b94d99bce6a
Wesley Moore <wes@wezm.net>
2026-03-26 20:25:53 +1000

    user/cosmic-app-library: new package

---
+++ user/cosmic-app-library/template.py
@@ -0,0 +1,34 @@
+pkgname = "cosmic-app-library"
+pkgver = "1.0.8"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "just", "pkgconf"]
+makedepends = ["rust-std", "libxkbcommon-devel"]
+depends = ["cosmic-icon-theme"]
+pkgdesc = "COSMIC application launcher"
+license = "GPL-3.0-only"
+url = "https://github.com/pop-os/cosmic-app-library"
+source = f"{url}/archive/refs/tags/epoch-{pkgver}.tar.gz"
+sha256 = "3c917c50f8af538760be98b9335fba8938c22523ff9044758ec346d2b998fbc9"
+# no tests
+options = ["!check"]
+
+
+def install(self):
+    self.install_bin(
+        f"target/{self.profile().triplet}/release/cosmic-app-library"
+    )
+    self.do(
+        "just",
+        "appid=com.system76.CosmicAppLibrary",
+        f"install-dir={self.chroot_destdir}/usr/share",
+        "install",
+        wrksrc=self.chroot_srcdir / "data",
+    )
+    self.do(
+        "just",
+        "appid=com.system76.CosmicAppLibrary",
+        f"install-dir={self.chroot_destdir}/usr/share",
+        "install",
+        wrksrc=self.chroot_srcdir / "data/icons",
+    )
