96299125686d9f093f9a7dda7705ddf83553edaf
Wesley Moore <wes@wezm.net>
2026-03-26 21:12:12 +1000

    user/cosmic-comp: new package

---
+++ user/cosmic-comp/template.py
@@ -0,0 +1,41 @@
+pkgname = "cosmic-comp"
+pkgver = "1.0.8"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = [
+    "fontconfig-devel",
+    "libdisplay-info-devel",
+    "libinput-devel",
+    "libseat-devel",
+    "libxkbcommon-devel",
+    "mesa-gbm-devel",
+    "oniguruma-devel",
+    "pixman-devel",
+    "rust-std",
+    "udev-devel",
+    "wayland-devel",
+    "zstd-devel",
+]
+depends = ["cosmic-icon-theme"]
+pkgdesc = "COSMIC compositor"
+license = "GPL-3.0-only"
+url = "https://github.com/pop-os/cosmic-comp"
+source = f"{url}/archive/refs/tags/epoch-{pkgver}.tar.gz"
+sha256 = "b8f473d5ccc25a3ff0756f4a62c0c7ba80ad574f00e40b278a97dc6fd8705fda"
+# no tests
+options = ["!check"]
+
+
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/cosmic-comp")
+    self.install_file(
+        "data/keybindings.ron",
+        "usr/share/cosmic/com.system76.CosmicSettings.Shortcuts/v1",
+        name="defaults",
+    )
+    self.install_file(
+        "data/tiling-exceptions.ron",
+        "usr/share/cosmic/com.system76.CosmicSettings.WindowRules/v1",
+        name="tiling_exception_defaults",
+    )
