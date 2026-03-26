d2fa551f9b25700348bcb3b01b76fad275705e90
Wesley Moore <wes@wezm.net>
2026-03-26 22:00:17 +1000

    user/cosmic-files: new package

---
+++ user/cosmic-files/template.py
@@ -0,0 +1,34 @@
+pkgname = "cosmic-files"
+pkgver = "1.0.8"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = ["rust-std", "glib-devel", "libxkbcommon-devel", "zstd-devel"]
+depends = ["cosmic-icon-theme", "gvfs", "xdg-utils"]
+pkgdesc = "COSMIC file manager"
+license = "GPL-3.0-only"
+url = "https://github.com/pop-os/cosmic-files"
+source = f"{url}/archive/refs/tags/epoch-{pkgver}.tar.gz"
+sha256 = "83d30eb99c5888bab4322361553796efee34b6f3af71a0845396378106d66e83"
+# examples/copy.rs fails to build
+options = ["!check"]
+
+
+def post_build(self):
+    self.cargo.build(["--package", "cosmic-files-applet"])
+
+
+def install(self):
+    self.install_bin(f"target/{self.profile().triplet}/release/cosmic-files")
+    self.install_bin(
+        f"target/{self.profile().triplet}/release/cosmic-files-applet"
+    )
+    self.install_file(
+        "target/xdgen/com.system76.CosmicFiles.metainfo.xml",
+        "usr/share/metainfo",
+    )
+    self.install_file(
+        "target/xdgen/com.system76.CosmicFiles.desktop",
+        "usr/share/applications",
+    )
+    self.install_files("res/icons/hicolor", "usr/share/icons")
