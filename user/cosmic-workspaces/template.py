badf50bcf73024a4cb149dc5f8758c4edc5b1c8f
Wesley Moore <wes@wezm.net>
2026-04-24 20:42:15 +1000

    user/cosmic-workspaces: new package

---
+++ user/cosmic-workspaces/template.py
@@ -0,0 +1,33 @@
+pkgname = "cosmic-workspaces"
+pkgver = "1.0.8"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = ["cargo-auditable", "pkgconf"]
+makedepends = [
+    "libinput-devel",
+    "libxkbcommon-devel",
+    "mesa-gbm-devel",
+    "rust-std",
+    "udev-devel",
+]
+depends = ["cosmic-icon-theme"]
+pkgdesc = "COSMIC notification daemon"
+license = "GPL-3.0-only"
+url = "https://github.com/pop-os/cosmic-workspaces-epoch"
+source = f"{url}/archive/refs/tags/epoch-{pkgver}.tar.gz"
+sha256 = "08c3cfc5c8cc51e35dac55576fcf5f0aea0fd30db0926ada0a85b02d59669232"
+# no tests
+options = ["!check"]
+
+
+def install(self):
+    self.install_bin(
+        f"target/{self.profile().triplet}/release/cosmic-workspaces"
+    )
+    self.install_file(
+        "data/com.system76.CosmicWorkspaces.desktop", "usr/share/applications"
+    )
+    self.install_file(
+        "data/com.system76.CosmicWorkspaces.svg",
+        "usr/share/icons/hicolor/scalable/apps",
+    )
